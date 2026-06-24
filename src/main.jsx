30f24fea0118c3d2ec951ab38840584341021c1c
Bruno Li <brunolb2002@gmail.com>
2026-06-24 18:05:01 -0300

    fix: pwa improvements

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if ('serviceWorker' in navigator) {
+  const hadController = Boolean(navigator.serviceWorker.controller)
+  let refreshing = false
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    if (hadController && !refreshing) {
+      refreshing = true
+      window.location.reload()
+    }
+  })
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
